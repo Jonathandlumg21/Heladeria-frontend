807fa8bec6bb0ca872898345f13528dca644bb35
Jonathan de Leon <jdeleona12@miumg.edu.gt>
2026-06-03 11:29:42 -0600

    config api para produccion en Render

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,17 +1,15 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || '/api',
 })
 
-// Adjuntar token en cada request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) config.headers.Authorization = `Bearer ${token}`
   return config
 })
 
-// Manejar 401 globalmente
 api.interceptors.response.use(
   (res) => res,
   (err) => {
