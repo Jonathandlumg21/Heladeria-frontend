810d9226b944e40c9465a2f9673417a35a133a6f
Jonathan de Leon <jdeleona12@miumg.edu.gt>
2026-07-15 19:19:25 -0600

    configuracion de impresora a rawbt 2.2

--- src/pages/Ventas.jsx
+++ src/pages/Ventas.jsx
@@ -177,6 +177,7 @@ export default function Ventas() {
   console.error('Error al imprimir:', e)
   toast.error('Error al generar el ticket')
 }
+}
 
   // ── Cobrar ──
   const cobrar = async () => {
